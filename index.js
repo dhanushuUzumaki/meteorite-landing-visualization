19b3e8d7af41d74481c8f92daf5f31239e851eda
Dhansuhu Uzumaki <a.p.dhanushu@gmail.com>
2018-03-29 19:41:06 +0530

    mvp

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const apiUrl = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json';
 
-const plot = (data) => {
-  data.nodes = data.nodes.map((d, index) => {
-    d['id'] = index;
-    return d;
-  });
+const plotData = data => {
+  const landingData = data.features.filter(d => d.geometry !== null);
   const margin = {
     top: 20,
     right: 20,
@@ -13,85 +10,116 @@ const plot = (data) => {
   };
   const width = Math.max((((window.innerWidth / 100) * 80) - margin.right - margin.left), 700);
   const height = ((window.innerHeight / 100) * 80) - margin.bottom - margin.top;
-  const svg = d3.select('svg')
-    .attr('width', width + margin.left + margin.right + 100)
-    .attr('height', height + margin.top + margin.bottom + 100)
-    .append('g')
-    .attr('transform',
-      `translate(${margin.left}, ${margin.top})`);
 
-  const simulation = d3.forceSimulation()
-    .force('link', d3.forceLink().id(function (d) { return d.id; }).distance(100).strength(1))
-    .force('charge', d3.forceManyBody())
-    .force('center', d3.forceCenter(width / 2, height / 2));
+  let initX,
+    mouseClicked = false,
+    s = 1,
+    rotated = 90,
+    mouse;
 
-  const dragstarted = d => {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-  }
+  const projection = d3.geoMercator()
+    .scale(153)
+    .translate([width / 2, height / 1.5])
+    .rotate([rotated, 0, 0]);
 
-  const dragged = d => {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-  }
+  const path = d3.geoPath().projection(projection);
 
-  const dragended = d => {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-  }
+  const zoom = d3.zoom()
+    .scaleExtent([1, 10])
+    .on("zoom", zoomed)
+    .on("end", zoomended);
 
-  const ticked = () => {
-    link
-      .attr('x1', d => d.source.x)
-      .attr('y1', d => d.source.y)
-      .attr('x2', d => d.target.x)
-      .attr('y2', d => d.target.y);
+  const svg = d3.select("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .on("wheel", function () {
+      initX = d3.mouse(this)[0];
+    })
+    .on("mousedown", function () {
+      if (s !== 1) return;
+      initX = d3.mouse(this)[0];
+      mouseClicked = true;
+    })
+    .call(zoom);
 
-    node
-      .attr('x', function (d) { return d.x = Math.max(5, Math.min(width - 5, d.x)); })
-      .attr('y', function (d) { return d.y = Math.max(5, Math.min(height - 5, d.y)); });
-  }
+  const g = svg.append("g");
 
-  const link = svg.append('g')
-    .attr('class', 'links')
-    .selectAll('line')
-    .data(data.links)
-    .enter().append('line')
-    .attr('stroke-width', function (d) { return Math.sqrt(d.value); });
 
-  const node = svg.append('g')
-    .attr('class', 'nodes')
-    .selectAll('.node-group')
-    .data(data.nodes)
+  function rotateMap(endX) {
+    projection.rotate([rotated + (endX - initX) * 360 / (s * width), 0, 0]);
+    g.selectAll('path').attr('d', path);
+    landings.data(landingData)
     .enter()
-    .append('g')
-    .append('foreignObject')
-    .attr('class', 'node-group')
-    .attr('width', '10')
-    .attr('height', '10')
-    .html(d => `<img class="flag flag-${d.code}" src="flags/blank.gif" />`)
-    .call(d3.drag()
-      .on('start', dragstarted)
-      .on('drag', dragged)
-      .on('end', dragended));
+    .append('circle')
+    .attr('r', 3)
+    .attr('fill', 'transparent')
+    .attr('stroke', 'black')
+    .attr('cx', d => projection([...d.geometry.coordinates])[0])
+    .attr('cy', d => projection([...d.geometry.coordinates])[1])
+    console.log(s, initX, rotated);
+  }
 
-  node.append('title')
-    .text(function (d) { return d.country; })
-    .exit();
+  function zoomended() {
+    if (s !== 1) return;
+    rotated = rotated + ((mouse[0] - initX) * 360 / (s * width));
+    mouseClicked = false;
+  }
 
-  simulation
-    .nodes(data.nodes)
-    .on('tick', ticked);
+  function zoomed() {
+    const t = [d3.event.transform.x, d3.event.transform.y];
+    s = d3.event.transform.k;
+    let h = 0;
 
-  simulation.force('link')
-    .links(data.links);
+    t[0] = Math.min(
+      (width / height) * (s - 1),
+      Math.max(width * (1 - s), t[0])
+    );
 
-}
+    t[1] = Math.min(
+      h * (s - 1) + h * s,
+      Math.max(height * (1 - s) - h * s, t[1])
+    );
+    g.attr("transform", "translate(" + t + ")scale(" + s + ")");
+    d3.selectAll(".boundary").style("stroke-width", 1 / s);
+    landings.data(landingData)
+    .enter()
+    .append('circle')
+    .attr('r', 3)
+    .attr('fill', 'transparent')
+    .attr('stroke', 'black')
+    .attr('cx', d => projection(d.geometry.coordinates)[0])
+    .attr('cy', d => projection(d.geometry.coordinates)[1])
+    mouse = d3.mouse(this);    
+    if (s === 1 && mouseClicked) {
+      rotateMap(mouse[0]);
+      return;
+    }
+  };
 
-const plotData = data => {
-  console.log(data);
+
+  d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function (error, world) {
+    if (error) return console.error(error);
+
+    //countries
+    g.append("g")
+      .attr("class", "boundary")
+      .selectAll("boundary")
+      .data(topojson.feature(world, world.objects.countries).features)
+      .enter().append("path")
+      .attr("d", path);
+
+  });
+
+  const landings_g = svg.append('g');
+  let landings = landings_g.selectAll('circle').data(landingData);
+  landings = landings.enter()
+    .append('circle')
+    .attr('r', 3)
+    .attr('fill', 'transparent')
+    .attr('stroke', 'black')
+    .attr('cx', d => projection(d.geometry.coordinates)[0])
+    .attr('cy', d => projection(d.geometry.coordinates)[1]);
+
 }
 
 const fetchData = () => {
