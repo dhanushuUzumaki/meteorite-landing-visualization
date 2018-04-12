0735731973b9ba41ad4ee528a8f3523d2db07a0c
Dhansuhu Uzumaki <a.p.dhanushu@gmail.com>
2018-04-12 15:12:37 +0530

    Bounded zoom implementation

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const apiUrl = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json';
-
+function isPointInPoly(poly, pt){
+  console.log(poly, pt);
+  for(var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+      ((poly[i].y <= pt.y && pt.y < poly[j].y) || (poly[j].y <= pt.y && pt.y < poly[i].y))
+      && (pt.x < (poly[j].x - poly[i].x) * (pt.y - poly[i].y) / (poly[j].y - poly[i].y) + poly[i].x)
+      && (c = !c);
+  return c;
+}
 const plotData = data => {
   const landingData = data.features.filter(d => d.geometry !== null);
   const margin = {
@@ -8,117 +15,93 @@ const plotData = data => {
     bottom: 10,
     left: 100
   };
-  const width = Math.max((((window.innerWidth / 100) * 80) - margin.right - margin.left), 700);
-  const height = ((window.innerHeight / 100) * 80) - margin.bottom - margin.top;
-
-  let initX,
-    mouseClicked = false,
-    s = 1,
-    rotated = 90,
-    mouse;
-
+  const width = Math.max((((window.innerWidth / 100) * 90) - margin.right - margin.left), 700);
+  const height = ((window.innerHeight / 100) * 90) - margin.bottom - margin.top;
+  let centered = null,
+      clickedPoint;
   const projection = d3.geoMercator()
-    .scale(153)
-    .translate([width / 2, height / 1.5])
-    .rotate([rotated, 0, 0]);
-
+    .translate([width / 2.2, height / 1.5]);
+  const planePath = d3.geoPath().projection(projection);
+  const svg = d3.select('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('class', 'map');
+  const mapG = svg.append('g').attr('class', 'boundary').on('click', () => console.log('clicked'));
   const path = d3.geoPath().projection(projection);
 
-  const zoom = d3.zoom()
-    .scaleExtent([1, 10])
-    .on("zoom", zoomed)
-    .on("end", zoomended);
-
-  const svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .on("wheel", function () {
-      initX = d3.mouse(this)[0];
-    })
-    .on("mousedown", function () {
-      if (s !== 1) return;
-      initX = d3.mouse(this)[0];
-      mouseClicked = true;
-    })
-    .call(zoom);
-
-  const g = svg.append("g");
-
-
-  function rotateMap(endX) {
-    projection.rotate([rotated + (endX - initX) * 360 / (s * width), 0, 0]);
-    g.selectAll('path').attr('d', path);
-    landings.data(landingData)
-    .enter()
-    .append('circle')
-    .attr('r', 3)
-    .attr('fill', 'transparent')
-    .attr('stroke', 'black')
-    .attr('cx', d => projection([...d.geometry.coordinates])[0])
-    .attr('cy', d => projection([...d.geometry.coordinates])[1])
-    console.log(s, initX, rotated);
-  }
-
-  function zoomended() {
-    if (s !== 1) return;
-    rotated = rotated + ((mouse[0] - initX) * 360 / (s * width));
-    mouseClicked = false;
-  }
-
-  function zoomed() {
-    const t = [d3.event.transform.x, d3.event.transform.y];
-    s = d3.event.transform.k;
-    let h = 0;
-
-    t[0] = Math.min(
-      (width / height) * (s - 1),
-      Math.max(width * (1 - s), t[0])
-    );
-
-    t[1] = Math.min(
-      h * (s - 1) + h * s,
-      Math.max(height * (1 - s) - h * s, t[1])
-    );
-    g.attr("transform", "translate(" + t + ")scale(" + s + ")");
-    d3.selectAll(".boundary").style("stroke-width", 1 / s);
-    landings.data(landingData)
-    .enter()
-    .append('circle')
-    .attr('r', 3)
-    .attr('fill', 'transparent')
-    .attr('stroke', 'black')
-    .attr('cx', d => projection(d.geometry.coordinates)[0])
-    .attr('cy', d => projection(d.geometry.coordinates)[1])
-    mouse = d3.mouse(this);    
-    if (s === 1 && mouseClicked) {
-      rotateMap(mouse[0]);
-      return;
+  const clicked = d => {
+    let x, y, k;
+    if (d && d !== centered) {
+      const centeroid = path.centroid(d);
+      const bounds = path.bounds(d);
+      const dx = bounds[1][0] - bounds[0][0];
+      const dy = bounds[1][1] - bounds[0][1];
+      x = (bounds[0][0] + bounds[1][0]) / 2;
+      y = (bounds[0][1] + bounds[1][1]) / 2;
+      k = Math.min(width / dx, height / dy);
+      centered = d;
+    } else {
+      x = width / 2;
+      y = height / 2;
+      k = 1;
+      centered = null;
     }
+    mapG.selectAll("path")
+      .classed("active", d => centered && d === centered);
+    if (centered !== null) {
+      mapG.selectAll("path")
+        .style("stroke-width", (0.75 / k) + "px");
+    }
+
+    mapG.transition()
+      .duration(750)
+      .attr('transform', `translate(${width / 2}, ${height / 2})scale(${k})translate(${-x}, ${-y})`)
+      .on('end', () => {
+        if (centered === null) {
+          mapG.selectAll("path")
+            .style("stroke-width", (0.75 / k) + "px");
+        }
+      });
+    let data;
+    mapG.selectAll('.landings').remove();
+    if (centered === null) {
+      data = landingData;
+    } else {
+      data = landingData.filter(p => d3.polygonContains(...d.geometry.coordinates, p.geometry.coordinates));    
+    }
+    mapG.selectAll('.landings')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('class', 'landings')
+        .attr('r', 3)
+        .attr('fill', 'transparent')
+        .attr('stroke', 'black')
+        .attr('cx', d => projection(d.geometry.coordinates)[0])
+        .attr('cy', d => projection(d.geometry.coordinates)[1]);
   };
 
 
-  d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function (error, world) {
-    if (error) return console.error(error);
+  d3.json('https://unpkg.com/world-atlas@1/world/110m.json', (error, topology) => {
+    mapG.selectAll('path')
+      .data(topojson.feature(topology, topology.objects.countries)
+        .features)
+      .enter()
+      .append('path')
+      .attr('d', path)
+      .on('click', clicked);
 
-    //countries
-    g.append("g")
-      .attr("class", "boundary")
-      .selectAll("boundary")
-      .data(topojson.feature(world, world.objects.countries).features)
-      .enter().append("path")
-      .attr("d", path);
-
+      mapG.selectAll('.landings')
+      .data(landingData)
+      .enter()
+      .append('circle')
+      .attr('class', 'landings')
+      .attr('r', 3)
+      .attr('fill', 'transparent')
+      .attr('stroke', 'black')
+      .attr('cx', d => projection(d.geometry.coordinates)[0])
+      .attr('cy', d => projection(d.geometry.coordinates)[1]);      
   });
-
-  const landings_g = svg.append('g');
-  let landings = landings_g.selectAll('circle').data(landingData);
-  landings = landings.enter()
-    .append('circle')
-    .attr('r', 3)
-    .attr('fill', 'transparent')
-    .attr('stroke', 'black')
-    .attr('cx', d => projection(d.geometry.coordinates)[0])
-    .attr('cy', d => projection(d.geometry.coordinates)[1]);
 
 }
 
